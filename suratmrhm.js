3ed5bc1fac50df77384bb807fae5e5f587ecefc0
mvlrfq <122500981+mvlrfq@users.noreply.github.com>
2026-02-16 21:20:53 +0700

    Update suratmrhm.js

--- suratmrhm.js
+++ suratmrhm.js
@@ -1,3 +1,27 @@
+var date = new Date();
+var days = ["Minggu", "Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu"];
+var months = ["Januari", "Februari", "Maret", "April", "Mei", "Juni", "Juli", "Agustus", "September", "Oktober", "November", "Desember"];
+var hours = date.getHours();
+var minutes = date.getMinutes();
+
+// Tambahkan awalan nol jika jam atau menit kurang dari 10
+if (hours < 10) {hours = "0" + hours;}
+if (minutes < 10) {minutes = "0" + minutes;}
+
+var day = days[date.getDay()];
+var dateNum = date.getDate();
+var month = months[date.getMonth()];
+var year = date.getFullYear();
+
+console.log(hours + "." + minutes + " WIB - " + day + ", " + dateNum + " " + month + " " + year);
+
+var element = document.querySelector("body");var watermark = document.createElement("div");
+
+// Setel teks watermark dan propertinya
+watermark.textContent = day + ", " + dateNum + " " + month + " " + year;
+watermark.style = "color:white;opacity:.5;font-size:10px;position:fixed;bottom:25px;left:25px;z-index:150";
+element.appendChild(watermark);
+
 initeksnimasi = teksnimasi.innerHTML;teksnimasi.innerHTML="";
 function katateksnimasi(){
   	new TypeIt("#teksnimasi", {
@@ -110,4 +134,5 @@ function keatas(){window.scrollTo(0, 0);}
             //initom.style="";
         }
     })
+
 })
